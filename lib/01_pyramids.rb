0afd8cf97e3d3eea841f6ecc048342ab1da44793
ArthurC2Pouce <agencecoupdepouce@gmail.com>
2019-01-10 15:43:20 +0100

    ajout du script pour faire le losange

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,4 +1,9 @@
-def pyramid(etages)
+#Half
+
+
+
+# Full
+def full_pyramid(etages)
   etages.times {|n|
     print ' ' * (etages - n)
     puts '#' * (2 * n + 1)
@@ -8,7 +13,26 @@ end
 def ask_etages
 	puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
 	etages = gets.chomp.to_i
-	return pyramid(etages)
+	return full_pyramid(etages)
 end
 
-ask_etages
+# WTF Pyramid
+
+def pyramid(etages)
+  etages.times {|n|
+    print ' ' * (etages - n)
+    puts '#' * (2 * n + 1)
+  }
+  etages = etages - 1
+  etages.times {|n|
+      print ' ' * (n + 2)
+      puts '#' * (2 * etages - 2 * n - 1)
+  }
+end
+
+def ask_etages
+    puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+    etages = gets.chomp.to_i
+    return pyramid(etages)
+end
+
